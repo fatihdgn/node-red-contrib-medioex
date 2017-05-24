a7bde05ef938b60aeaa20bfb34a6b0d749c20b7d
fatihdgn <fth.dgn.369@gmail.com>
2017-05-24 13:50:42 +0300

    Project created.

---
+++ nodes/analog-input-init/analog-input-init.js
@@ -0,0 +1,13 @@
+var MedIOEx = require("MedIOEx-js");
+
+module.exports = function(RED) {
+  function analogInputInit(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+    node.on("input", function(msg){
+      MedIOEx.analogInputInit();
+      node.send(msg);
+    });
+  };
+  RED.nodes.registerType("analog-input-init", analogInputInit);
+}
